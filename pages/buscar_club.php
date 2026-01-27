e7eefd668b9518700740933ffd0ae77646848f1d
Luis <luis@MacBook-Pro-de-Luis.local>
2026-01-27 19:50:25 -0300

    qa buscar_club1

--- pages/buscar_club.php
+++ pages/buscar_club.php
@@ -9,7 +9,7 @@
   <link rel="manifest" href="/manifest.json">
   <meta name="theme-color" content="#003366">
   <link rel="apple-touch-icon" href="/assets/icons/icon-192.png">
-  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
   <style>
     /* Fondo corporativo de Cancha */
