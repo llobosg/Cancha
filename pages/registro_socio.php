0bba90218476833799aaad4556d75a63859bf594
llobosg <llobos@gltcomex.com>
2026-04-15 14:06:26 -0400

    rollback registro_socio, 42

--- pages/registro_socio.php
+++ pages/registro_socio.php
@@ -400,9 +400,9 @@ while ($row = $stmt_regiones->fetch()) {
             window.location.href = '/torneo_pair.php?slug=<?= $torneo_slug ?>&code=<?= $torneo_code ?>';
           <?php else: ?>
             if (data.club_slug && data.club_slug.trim() !== '') {
-              window.location.href = 'verificar_socio.php?club=' + encodeURIComponent(data.club_slug);
+              window.location.href = 'verificar_codigo_socio.php?club=' + encodeURIComponent(data.club_slug);
             } else {
-              window.location.href = 'verificar_socio.php?id_socio=' + encodeURIComponent(data.id_socio);
+              window.location.href = 'verificar_codigo_socio.php?id_socio=' + encodeURIComponent(data.id_socio);
             }
           <?php endif; ?>
         } else {
